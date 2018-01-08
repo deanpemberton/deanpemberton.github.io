ef67657756c08076d6851f6b863bc162997cf1a3
Dean Pemberton <dean@deanpemberton.com>
2018-01-08 00:01:54 +0000

    Updated 'js/site.js' via CloudCannon

--- js/site.js
+++ js/site.js
@@ -28,10 +28,27 @@ var JekyllMaps = function () {
 }
 
 JekyllMaps.prototype._createMap = function() {
+    
+    var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+    
   this.map = L.map( 'map' ).setView( [0, 0], 1 );
   L.tileLayer( '{{ site.map-tileset }}', {
     attribution: '{{ site.map-credits }}'
   }).addTo( this.map );
-  this.geojson = L.geoJson( this.data ).addTo( this.map );
+  
+ this.geojson =  L.geoJSON(this.data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
+  
+//  this.geojson = L.geoJson( this.data ).addTo( this.map );
   this.map.fitBounds(this.geojson.getBounds());
 }
